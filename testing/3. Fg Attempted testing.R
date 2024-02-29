4f205222b7290931a4dcb04b8889b7fbb36539f5
Antonio Maldonado Ostios <amaldonado@caesars.com>
2024-02-29 11:24:04 +0000

    Added scripts to test the bayesian coefficients

---
+++ testing/3. Fg Attempted testing.R
@@ -0,0 +1,114 @@
+library(arm)
+
+allPlayers <- read.csv("C:\\czrs-ds-models\\nba-player-props\\data\\allPlayers.csv")
+
+BASE_DIR <- "C:\\czrs-ds-models\\nba-player-props\\testing\\"
+
+javaPreds <- read.csv("C:\\czrs-ds-models\\nba-player-props\\testing\\fgAttemptedPerMin.csv")
+javaPreds <- subset(javaPreds, javaPreds$seasonYear > 2017 & javaPreds$target >= 0)
+javaPreds <- merge(javaPreds, allPlayers[c("GameId", "PlayerId", "Team", "HomeTeam", "AwayTeam",  "matchSpread", "totalPoints")])
+
+javaPreds$homeExp <- (javaPreds$totalPoints - javaPreds$matchSpread) / 2
+javaPreds$awayExp <- javaPreds$totalPoints - javaPreds$homeExp
+
+javaPreds$ownExp <- ifelse(javaPreds$HomeTeam == javaPreds$Team, javaPreds$homeExp, javaPreds$awayExp)
+javaPreds$oppExp <- ifelse(javaPreds$HomeTeam == javaPreds$Team, javaPreds$awayExp, javaPreds$homeExp)
+
+javaPreds <- subset(javaPreds, !is.na(javaPreds$totalPoints))
+
+javaPreds$fgExp <- javaPreds$targetPredicted * javaPreds$minPlayed
+javaPreds$fgResid <- javaPreds$fgAttempted - javaPreds$fgExp
+
+plot.ts(javaPreds$target[javaPreds$PlayerId == 3992])
+lines(javaPreds$targetPredicted[javaPreds$PlayerId == 3992], col = "red")
+
+
+javaPreds$resid <- javaPreds$target - javaPreds$targetPredicted
+
+binnedplot(javaPreds$targetPredicted[javaPreds$PlayerId == 3992], 
+           javaPreds$resid[javaPreds$PlayerId == 3992])
+
+binnedplot(javaPreds$targetPredicted, javaPreds$resid)
+binnedplot(javaPreds$ownExp, javaPreds$resid)
+binnedplot(javaPreds$ownExp, javaPreds$fgResid)
+
+
+binnedplot(javaPreds$threeExp, javaPreds$threeResid)
+binnedplot(javaPreds$ownExp, javaPreds$threeResid)
+
+binnedplot(javaPreds$threeExp, javaPreds$threeResid)
+binnedplot(javaPreds$totalPoints, javaPreds$threeResid)
+
+binnedplot(javaPreds$fgAttempted, javaPreds$ftResid)
+binnedplot(javaPreds$minPlayed, javaPreds$ftResid)
+
+binnedplot(javaPreds$playerCoef, javaPreds$ftResid)
+
+binnedplot(javaPreds$targetPredicted[javaPreds$seasonYear == 2018], javaPreds$resid[javaPreds$seasonYear == 2018])
+binnedplot(javaPreds$targetPredicted[javaPreds$seasonYear == 2019], javaPreds$resid[javaPreds$seasonYear == 2019])
+binnedplot(javaPreds$targetPredicted[javaPreds$seasonYear == 2020], javaPreds$resid[javaPreds$seasonYear == 2020])
+binnedplot(javaPreds$targetPredicted[javaPreds$seasonYear == 2021], javaPreds$resid[javaPreds$seasonYear == 2021])
+binnedplot(javaPreds$targetPredicted[javaPreds$seasonYear == 2022], javaPreds$resid[javaPreds$seasonYear == 2022])
+binnedplot(javaPreds$targetPredicted[javaPreds$seasonYear == 2023], javaPreds$resid[javaPreds$seasonYear == 2023])
+
+binnedplot(javaPreds$targetPredicted[javaPreds$numbOfGames >50], javaPreds$resid[javaPreds$numbOfGames>50])
+
+binnedplot(javaPreds$targetPredicted[javaPreds$numbOfGames <10], javaPreds$resid[javaPreds$numbOfGames<10])
+binnedplot(javaPreds$playerCoef[javaPreds$numbOfGames <10], javaPreds$resid[javaPreds$numbOfGames<10])
+binnedplot(javaPreds$targetPredicted[javaPreds$numbOfGames ==1], javaPreds$resid[javaPreds$numbOfGames==1])
+
+binnedplot(javaPreds$targetPredicted[javaPreds$seasonYear == 2023 & javaPreds$numbOfGames < 10], 
+           javaPreds$resid[javaPreds$seasonYear == 2023& javaPreds$numbOfGames < 10])
+binnedplot(javaPreds$targetPredicted[javaPreds$seasonYear == 2023 & javaPreds$numbOfGames >10], 
+           javaPreds$resid[javaPreds$seasonYear == 2023& javaPreds$numbOfGames > 10])
+binnedplot(javaPreds$playerCoef[javaPreds$seasonYear == 2018], javaPreds$resid[javaPreds$seasonYear == 2018])
+binnedplot(javaPreds$playerCoef[javaPreds$seasonYear == 2019], javaPreds$resid[javaPreds$seasonYear == 2019])
+binnedplot(javaPreds$playerCoef[javaPreds$seasonYear == 2020], javaPreds$resid[javaPreds$seasonYear == 2020])
+binnedplot(javaPreds$playerCoef[javaPreds$seasonYear == 2021], javaPreds$resid[javaPreds$seasonYear == 2021])
+binnedplot(javaPreds$playerCoef[javaPreds$seasonYear == 2022], javaPreds$resid[javaPreds$seasonYear == 2022])
+binnedplot(javaPreds$playerCoef[javaPreds$seasonYear == 2023], javaPreds$resid[javaPreds$seasonYear == 2023])
+
+summary(javaPreds$resid)
+
+javaPreds$threePred <- javaPreds$fgAttempted * javaPreds$targetPredicted
+javaPreds$threeResid <- javaPreds$threeAttempted - javaPreds$threePred
+
+javaPreds$fgPredicted <- javaPreds$targetPredicted * javaPreds$minPlayed
+javaPreds$fgResid <- javaPreds$fgPredicted - javaPreds$fgAttempted
+
+binnedplot(javaPreds$targetPredicted, javaPreds$resid)
+binnedplot(javaPreds$fgPredicted, javaPreds$fgResid)
+
+binnedplot(javaPreds$fgAttempted, javaPreds$resid)
+binnedplot(javaPreds$threeAttempted, javaPreds$resid)
+
+binnedplot(javaPreds$targetPredicted[javaPreds$numbOfGames > 20], javaPreds$resid[javaPreds$numbOfGames > 20])
+binnedplot(javaPreds$targetPredicted[javaPreds$numbOfGames < 5], javaPreds$resid[javaPreds$numbOfGames < 5])
+
+binnedplot(javaPreds$minPlayed, javaPreds$resid)
+binnedplot(javaPreds$playerCoef, javaPreds$resid)
+binnedplot(javaPreds$playerCoef[javaPreds$numbOfGames > 5], javaPreds$resid[javaPreds$numbOfGames > 5])
+
+
+
+
+#
+
+library(splines)
+
+model <- glm(FT.Attempted ~ log(ftExp) + pmax(fgAttempted - 20, 0)+ pmax(fgAttempted - 10, 0)+ minPlayed + pmin(playerCoef, 0) , data = javaPreds, family = "poisson") 
+summary(model)
+
+javaPreds$pred <- predict(model, javaPreds, type = "response")
+javaPreds$resid <- javaPreds$pred - javaPreds$FT.Attempted
+
+binnedplot(javaPreds$pred, javaPreds$resid)
+binnedplot(javaPreds$fgAttempted, javaPreds$resid)
+binnedplot(javaPreds$minPlayed, javaPreds$resid)
+
+binnedplot(javaPreds$pred[javaPreds$fgAttempted>7], javaPreds$resid[javaPreds$fgAttempted>7])
+binnedplot(javaPreds$pred[javaPreds$fgAttempted<4], javaPreds$resid[javaPreds$fgAttempted<4])
+binnedplot(javaPreds$pred[javaPreds$fgAttempted<2], javaPreds$resid[javaPreds$fgAttempted<2])
+binnedplot(javaPreds$pred[javaPreds$fgAttempted<6], javaPreds$resid[javaPreds$fgAttempted<6])
+binnedplot(javaPreds$playerCoef[javaPreds$fgAttempted<6], javaPreds$resid[javaPreds$fgAttempted<6])
+binnedplot(javaPreds$playerCoef, javaPreds$resid)
---
+++ testing/Join predictions.R
@@ -0,0 +1,52 @@
+library(arm)
+
+BASE_DIR <- "C:\\czrs-ds-models\\nba-player-props\\"
+COEFS_DIR <- paste0(BASE_DIR, "testing\\")
+
+players <- read.csv(paste0(BASE_DIR, "data\\allPlayers.csv"))
+players <- unique(players[c("GameId", "Name", "PlayerId", "Team", "seasonYear")])
+
+fgPred <- read.csv(paste0(COEFS_DIR, "fgAttemptedPerMin.csv"))[c("seasonYear", "GameId", "PlayerId", "minPlayed", "priorForPlayer", "playerCoef")]
+threeProp <- read.csv(paste0(COEFS_DIR, "threeProp.csv"))[c("GameId", "PlayerId", "priorForPlayer", "playerCoef")]
+twoPerc <- read.csv(paste0(COEFS_DIR, "twoPerc.csv"))[c("GameId", "PlayerId", "priorForPlayer", "playerCoef")]
+threePerc <- read.csv(paste0(COEFS_DIR, "threePerc.csv"))[c("GameId", "PlayerId", "priorForPlayer", "playerCoef")]
+assists <- read.csv(paste0(COEFS_DIR, "assistsPerMin.csv"))[c("GameId", "PlayerId", "priorForPlayer", "playerCoef")]
+steals <- read.csv(paste0(COEFS_DIR, "stealsPerMin.csv"))[c("GameId", "PlayerId", "priorForPlayer", "playerCoef")]
+blocks <- read.csv(paste0(COEFS_DIR, "blocksPerMin.csv"))[c("GameId", "PlayerId", "priorForPlayer", "playerCoef")]
+rebounds <- read.csv(paste0(COEFS_DIR, "reboundsPerMin.csv"))[c("GameId", "PlayerId", "priorForPlayer", "playerCoef")]
+ofReboundsProp <- read.csv(paste0(COEFS_DIR, "ofRebProp.csv"))[c("GameId", "PlayerId", "priorForPlayer", "playerCoef")]
+turnovers <- read.csv(paste0(COEFS_DIR, "turnoversPerMin.csv"))[c("GameId", "PlayerId", "priorForPlayer", "playerCoef")]
+personalFouls <- read.csv(paste0(COEFS_DIR, "personalFoulsPerMin.csv"))[c("GameId", "PlayerId", "priorForPlayer", "playerCoef")]
+ftsAttemptedPerMin <- read.csv(paste0(COEFS_DIR, "ftsAttemptedPerMin.csv"))[c("GameId", "PlayerId", "priorForPlayer", "playerCoef")]
+ftPerc <- read.csv(paste0(COEFS_DIR, "ftPerc.csv"))[c("GameId", "PlayerId", "priorForPlayer", "playerCoef")]
+
+colnames(fgPred)[5:6] <- c("fgPrior", "fgPlayerCoef")
+colnames(threeProp)[3:4] <- c( "threePropPrior", "threePropPlayerCoef")
+colnames(twoPerc)[3:4] <- c( "twoPercPrior", "twoPercPlayerCoef")
+colnames(threePerc)[3:4] <- c("threePercPrior", "threePercPlayerCoef")
+colnames(assists)[3:4] <- c("assistsPrior", "assistsPlayerCoef")
+colnames(steals)[3:4] <- c("stealsPrior", "stealsPlayerCoef")
+colnames(blocks)[3:4] <- c("blocksPrior", "blocksPlayerCoef")
+colnames(rebounds)[3:4] <- c("reboundsPrior", "reboundsPlayerCoef")
+colnames(ofReboundsProp)[3:4] <- c("ofReboundsPropPrior", "ofReboundsPropPlayerCoef")
+colnames(turnovers)[3:4] <- c("turnoversPrior", "turnoversPlayerCoef")
+colnames(personalFouls)[3:4] <- c("personalFoulsPrior", "personalFoulsPlayerCoef")
+colnames(ftsAttemptedPerMin)[3:4] <- c("ftsAttemptedPerMinPrior", "ftsAttemptedPerMinPlayerCoef")
+colnames(ftPerc)[3:4] <- c("ftsPercPrior", "ftsPercPlayerCoef")
+
+allPreds <- merge(fgPred, threeProp, by = c("GameId", "PlayerId"))
+allPreds <- merge(allPreds, twoPerc, by = c("GameId", "PlayerId"))
+allPreds <- merge(allPreds, threePerc, by = c("GameId", "PlayerId"))
+allPreds <- merge(allPreds, assists, by = c("GameId", "PlayerId"))
+allPreds <- merge(allPreds, steals, by = c("GameId", "PlayerId"))
+allPreds <- merge(allPreds, blocks, by = c("GameId", "PlayerId"))
+allPreds <- merge(allPreds, rebounds, by = c("GameId", "PlayerId"))
+allPreds <- merge(allPreds, ofReboundsProp, by = c("GameId", "PlayerId"))
+allPreds <- merge(allPreds, turnovers, by = c("GameId", "PlayerId"))
+allPreds <- merge(allPreds, personalFouls, by = c("GameId", "PlayerId"))
+allPreds <- merge(allPreds, ftsAttemptedPerMin, by = c("GameId", "PlayerId"))
+allPreds <- merge(allPreds, ftPerc, by = c("GameId", "PlayerId"))
+
+allPreds <- merge(allPreds, players, by = c("GameId", "PlayerId"), all.x = T)
+
+write.csv(allPreds, paste0(COEFS_DIR, "allPreds.csv"))
