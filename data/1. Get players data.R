5db1e2b3e02273ae944c75955f7860bb407412c0
amaldonado <amaldonado@caesars.com>
2024-03-18 14:40:32 +0000

    Added logic to clean data from espn

--- data/1. Get players data.R
+++ data/1. Get players data.R
@@ -20,9 +20,44 @@ events = dbGetQuery(con, "SELECT * FROM espn_games")
 players = dbGetQuery(con, "SELECT * FROM espn_boxscores")
 sbrOdds <- dbGetQuery(con, "SELECT * FROM nba_sbr_odds")
 
-mappedOdds <- mapSbrDataToEspn(eventsData = events, sbrData = sbrOdds, baseDir = BASE_DIR)
+#Clean espn data
+players <- players[,-which(colnames(players) == "seasonYear")]
+players <- merge(players, events[c("GameId", "seasonYear")], by = "GameId")
+players <- unique(players)
 
+players$id <- paste0(players$GameId, "_", players$PlayerId)
+dups <- data.frame(table(players$id))
+
+dupsRows <- subset(dups, dups$Freq > 1)
+noDups <- subset(dups, dups$Freq == 1)
+
+cleanDups <- data.frame()
+for(i in unique(dupsRows$Var1)){
+  cleanDups <- rbind(cleanDups, subset(players, players$id %in% i)[1,])
+}
+
+players <- rbind(cleanDups, subset(players, players$id %in% noDups$Var1))
+rm(cleanDups, noDups, dups, dupsRows)
+
+#Map to odds
+mappedOdds <- mapSbrDataToEspn(eventsData = events, sbrData = sbrOdds, baseDir = BASE_DIR)
+dups <- data.frame(table(mappedOdds$GameId))
+
+dupsRows <- subset(dups, dups$Freq > 1)
+noDups <- subset(dups, dups$Freq == 1)
+
+cleanDups <- data.frame()
+for(i in unique(dupsRows$Var1)){
+  cleanDups <- rbind(cleanDups, subset(mappedOdds, mappedOdds$GameId %in% i)[1,])
+}
+
+mappedOdds <- rbind(cleanDups, subset(mappedOdds, mappedOdds$GameId %in% noDups$Var1))
+
+#Clean odds data
 players <- merge(players, events[c("GameId", "AwayTeam", "HomeTeam", "Date")], by = "GameId", all.x = T)
 players <- merge(players, mappedOdds[c("GameId", "matchSpread", "totalPoints")], by = "GameId", all.x = T)
+
+players$id <- paste0(players$GameId, "_", players$PlayerId)
+View(table(players$id))
 
 write.csv(players, paste0(BASE_DIR, "data\\allPlayers.csv"))
