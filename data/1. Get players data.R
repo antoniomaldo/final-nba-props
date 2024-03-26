1a9548ab219a95088c54ac18442fcd592328ffd6
amaldonado <amaldonado@caesars.com>
2024-03-26 10:25:09 +0000

    Added average minutes played in season to the data

--- data/1. Get players data.R
+++ data/1. Get players data.R
@@ -1,6 +1,7 @@
 library(DBI)
 library(bigrquery)
 library(stringr)
+library(zoo)
 
 BASE_DIR <- "C:\\czrs-ds-models\\nba-player-props\\"
 
@@ -15,10 +16,28 @@ con <- dbConnect(
 )
 
 #dbListTables(con)
+# 
+# events = dbGetQuery(con, "SELECT * FROM espn_games")
+# players = dbGetQuery(con, "SELECT * FROM espn_boxscores")
+# sbrOdds <- dbGetQuery(con, "SELECT * FROM nba_sbr_odds")
 
-events = dbGetQuery(con, "SELECT * FROM espn_games")
-players = dbGetQuery(con, "SELECT * FROM espn_boxscores")
-sbrOdds <- dbGetQuery(con, "SELECT * FROM nba_sbr_odds")
+BASE_DATA_DIR = "C:/Users/amaldonado/Documents/NBA/data/"
+
+loadDataInDirectory <- function(dir){
+  folders <- list.files(dir, full.names = T)  
+  df <- data.frame()
+  for(folder in folders){
+    season <- str_remove(folder, dir)
+    seasonData <- do.call(rbind, lapply(list.files(folder, full.names = T), read.csv))
+    seasonData$seasonYear = season
+    df <- rbind(df, seasonData)
+  }
+  return(df)
+}
+
+sbrOdds <- loadDataInDirectory(paste0(BASE_DATA_DIR, "sbr-odds/"))
+players <- loadDataInDirectory(paste0(BASE_DATA_DIR, "espn/Players/"))
+events <- loadDataInDirectory(paste0(BASE_DATA_DIR, "espn/Boxscores/"))
 
 #Clean espn data
 players <- players[,-which(colnames(players) == "seasonYear")]
@@ -39,6 +58,24 @@ for(i in unique(dupsRows$Var1)){
 players <- rbind(cleanDups, subset(players, players$id %in% noDups$Var1))
 rm(cleanDups, noDups, dups, dupsRows)
 
+#Add cum data
+players <- merge(players, events[c("GameId", "Date")])
+
+playedData <- subset(players, players$Min > 0)
+
+playedData <- playedData[order(playedData$PlayerId, playedData$Date),]
+
+playedData$cumMinInSeason <- unlist(aggregate(Min ~ seasonYear + PlayerId, playedData, FUN = function(x){cumsum(c(0, x[-length(x)]))})$Min)
+playedData$cumGamesPlayedInSeason <- unlist(aggregate(Min ~ seasonYear + PlayerId, playedData, FUN = function(x){(0: (length(x) - 1))})$Min)
+playedData$averageMinutesInSeason <- playedData$cumMinInSeason / playedData$cumGamesPlayedInSeason
+
+
+players <- merge(players, playedData[c("GameId", "PlayerId", "cumMinInSeason", "cumGamesPlayedInSeason", "averageMinutesInSeason", "Date")], all.x = T)
+players <- players[order(players$PlayerId, players$Date),]
+players$averageMinutesInSeason[!is.na(players$cumGamesPlayedInSeason) & players$cumGamesPlayedInSeason == 0] <- -1
+
+players$averageMinutesInSeason <- na.locf(players$averageMinutesInSeason)
+
 #Map to odds
 mappedOdds <- mapSbrDataToEspn(eventsData = events, sbrData = sbrOdds, baseDir = BASE_DIR)
 dups <- data.frame(table(mappedOdds$GameId))
@@ -54,10 +91,10 @@ for(i in unique(dupsRows$Var1)){
 mappedOdds <- rbind(cleanDups, subset(mappedOdds, mappedOdds$GameId %in% noDups$Var1))
 
 #Clean odds data
-players <- merge(players, events[c("GameId", "AwayTeam", "HomeTeam", "Date")], by = "GameId", all.x = T)
+players <- merge(players, events[c("GameId", "AwayTeam", "HomeTeam")], by = "GameId", all.x = T)
 players <- merge(players, mappedOdds[c("GameId", "matchSpread", "totalPoints")], by = "GameId", all.x = T)
 
 players$id <- paste0(players$GameId, "_", players$PlayerId)
 View(table(players$id))
 
-write.csv(players, paste0(BASE_DIR, "data\\allPlayers.csv"))
+write.csv(players, paste0(BASE_DIR, "data/allPlayers.csv"))
